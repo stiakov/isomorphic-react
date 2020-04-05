e4184fb991220f556dd0d01f9ed7aca0347074c9
Santiago Torres G <thdealer@gmail.com>
2020-04-05 11:45:40 -0500

    Reset Jest

---
+++ src/components/__tests__/QuestionDetail.js
@@ -0,0 +1,5 @@
+describe('The Question detail component', () => {
+  it('Should not regress', () => {
+    
+  })
+});
---
+++ src/components/__tests__/QuestionList.spec.js
@@ -0,0 +1,13 @@
+describe("The question List", () => {
+  beforeEach(() => {
+    console.log("before each!!!");
+  });
+
+  it("Should display a list of items", () => {
+    expect(2 + 2).toEqual(4);
+  });
+  
+  beforeAll(() => {
+    console.log("before All!!!");
+  });
+});
