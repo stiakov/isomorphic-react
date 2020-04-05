49b8bf905df024e81c8918a858698f16844e5040
Santiago Torres G <thdealer@gmail.com>
2020-04-05 16:49:50 -0500

    Add Component test -> Anti Regression

--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -1,4 +1,30 @@
+import { mapStateToProps } from '../QuestionDetail';
+
 describe('The Question detail component', () => {
+  
+  // REGRESION TEST
+  describe('The container Element', () => {
+    describe('mapStateToProps', () => {
+      it('should map the state to props correctly', () => {
+        
+        const sampleQuestion = {
+          question_id: 42,
+          body: 'Space is big',
+        };
+        const appState = {
+          questions: [sampleQuestion],
+        };
+        const ownProps = {
+          question_id: 42,
+        };
+        const componentState = mapStateToProps(appState, ownProps);
+        expect(componentState).toEqual(sampleQuestion);
+      });
+      
+    })
+    
+  })
+  
   it('Should not regress', () => {
     
   })
