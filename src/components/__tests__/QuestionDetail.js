c23c359e2339edbb4d5c51a52c30be3dafd2acd5
Santiago Torres G <thdealer@gmail.com>
2020-04-05 21:02:41 -0500

    Add unit test

--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -1,15 +1,15 @@
-import { mapStateToProps } from '../QuestionDetail';
+import { mapStateToProps, QuestionDetailDisplay } from "../QuestionDetail";
+import renderer from "react-test-renderer";
+import React from "react";
 
-describe('The Question detail component', () => {
-  
-  // REGRESION TEST
-  describe('The container Element', () => {
-    describe('mapStateToProps', () => {
-      it('should map the state to props correctly', () => {
-        
+describe("The Question detail component", () => {
+  // REGRESSION TEST
+  describe("The container Element", () => {
+    describe("mapStateToProps", () => {
+      it("should map the state to props correctly", () => {
         const sampleQuestion = {
           question_id: 42,
-          body: 'Space is big',
+          body: "Space is big",
         };
         const appState = {
           questions: [sampleQuestion],
@@ -20,12 +20,21 @@ describe('The Question detail component', () => {
         const componentState = mapStateToProps(appState, ownProps);
         expect(componentState).toEqual(sampleQuestion);
       });
-      
-    })
-    
-  })
-  
-  it('Should not regress', () => {
-    
-  })
+    });
+  });
+
+  describe("The display element", () => {
+    it("Should not regress", () => {
+      const tree = renderer.create(
+        <QuestionDetailDisplay
+          title="The meaning of life"
+          body="42"
+          answer_count={0}
+          tags={["hitchhiking"]}
+        />
+      );
+
+      expect(tree.toJSON()).toMatchSnapshot();
+    });
+  });
 });
